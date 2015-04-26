0909340803d78d9dd00b2baaac128d78912809c5
murphyky <kyle-murphy89@live.com>
2015-04-26 23:01:26 +0100

    testing deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ssh root@kyle-murphy.co.uk 'cd /var/www/node-blog; git pull'
+ssh root@kyle-murphy.co.uk 'service nodeblog stop; cd /var/www/node-blog; git pull; npm install; service node-blog start'
